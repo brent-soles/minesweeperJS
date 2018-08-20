ef8edd45564ab00cd1309367d8d3a10d5b8f03e7
brent-soles <b.d.soles@gmail.com>
2018-08-20 13:10:12 -0700

    Iteration 2: Nested array and print function
    Signed-off-by: brent-soles <b.d.soles@gmail.com>

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -2,13 +2,35 @@ const blankLine = ' | | ';
 const guessLine = '1| | ';
 const bombLine = ' |B| ';
 
-console.log('Empty minesweeper board:');
-for(let i = 0; i < 3; i++){
-  console.log(blankLine);
-}
+const mineField = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
 
-console.log("");
-console.log('Filled minesweeper board:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+
+const printBoard = (board) => {
+  console.log('Current board:');
+  for(let i = 0; i < board.length; i++){
+    console.log(board[i].join(' | '));
+  }
+};
+
+
+printBoard(mineField);
+
+mineField[0][1] = '1';
+mineField[2][2] = 'B';
+
+printBoard(mineField);
+
+// console.log('Empty minesweeper board:');
+// for(let i = 0; i < 3; i++){
+//   console.log(blankLine);
+// }
+
+// console.log("");
+// console.log('Filled minesweeper board:');
+// console.log(guessLine);
+// console.log(bombLine);
+// console.log(blankLine);
