9046a99ee809785ce918a36ab59a89be888c77d7
brent-soles <b.d.soles@gmail.com>
2018-08-21 10:15:36 -0700

    Iteration 3: Added dynamic board iteration
    Signed-off-by: brent-soles <b.d.soles@gmail.com>

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -2,13 +2,39 @@ const blankLine = ' | | ';
 const guessLine = '1| | ';
 const bombLine = ' |B| ';
 
-const mineField = [
+const playerField = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
 
+/* 
+Generates a board with # or rows/columns
+returns the created board;
+*/
+const generateBoard = (rows, cols) => {
+  
+  if(!rows || !cols){
+    console.log('generateBoard: invalid args');
+    return;
+  }
 
+  let resultBoard = [];
+
+  for(let i = 0; i < rows; i++){
+    resultBoard.push([]);
+    for(let j = 0; j < cols; j++){
+      resultBoard[i].push(' ');
+    }
+  }
+
+  return resultBoard;
+}
+
+/**
+ * Prints the board
+ * @param {board} board 
+ */
 const printBoard = (board) => {
   console.log('Current board:');
   for(let i = 0; i < board.length; i++){
@@ -16,10 +42,27 @@ const printBoard = (board) => {
   }
 };
 
+/**
+ * populates a board with mines
+ * @param {*} mineField 
+ */
+const populateBombs = (mineField) => {
+  
+  if(!mineField){
+    console.log('populateBombs: empty array passed');
+    return 0;
+  }
+  
+  mineField.forEach((mineRow) => {
+    let index = Math.floor(Math.random() * (mineRow.rows));
+    mineRow[index] = 'B';
+  });
 
-printBoard(mineField);
+};
 
-mineField[0][1] = '1';
-mineField[2][2] = 'B';
+const mineBoard = generateBoard(3, 3);
+populateBombs(mineBoard);
+printBoard(mineBoard);
 
-printBoard(mineField);
+const playerBoard = generateBoard(3, 3);
+printBoard(playerBoard);
